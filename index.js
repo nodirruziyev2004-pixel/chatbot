f4bd7d4b06be7df650480b49e028cf232701766d
nodirruziyev2004-pixel <nodirruziyev2004@gmail.com>
2026-04-14 01:05:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
-const { Telegraf } = require("telegraf");
+bot.command("quti", (ctx) => {
+  const id = ctx.from.id;
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+  if (!users[id]) {
+    users[id] = { coin: 100, freeBox: true };
+  }
 
-bot.start((ctx) => {
-  ctx.reply("🚀 Bot ishlayapti!");
+  // Bepul quti
+  if (users[id].freeBox) {
+    users[id].freeBox = false;
+    return openBox(ctx, id, true);
+  }
+
+  // Coin tekshirish
+  if (users[id].coin < 50) {
+    return ctx.reply("❌ Quti ochish uchun 50 coin kerak");
+  }
+
+  users[id].coin -= 50;
+  openBox(ctx, id, false);
 });
 
-bot.launch();
+// Quti logikasi
+function openBox(ctx, id, isFree) {
+  let rand = Math.random() * 100;
+
+  let message = isFree
+    ? "🎁 Bepul quti ochdingiz!\n\n"
+    : "🎁 Quti ochdingiz!\n\n";
+
+  // 50% coin
+  if (rand < 50) {
+    let coin = Math.floor(Math.random() * 700) + 100;
+    users[id].coin += coin;
+    return ctx.reply(message + `💰 Siz ${coin} coin yutdingiz!`);
+  }
+
+  // 20% pul
+  else if (rand < 70) {
+    let money = Math.floor(Math.random() * 4000) + 1000;
+    return ctx.reply(message + `💵 Siz ${money} so‘m yutdingiz!`);
+  }
+
+  // 5% premium
+  else if (rand < 75) {
+    return ctx.reply(message + "💎 Tabriklaymiz! Telegram Premium yutdingiz!");
+  }
+
+  // 25% yutqazish
+  else {
+    return ctx.reply(message + "😢 Afsus, hech narsa chiqmadi");
+  }
+}
